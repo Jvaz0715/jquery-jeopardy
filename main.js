1a46c1bb947b35c5392dcee478f58209282fa0df
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-19 13:12:20 -0400

    Starts using jquery

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-// links to jeopardy.js to target questions
+// using jquery, get the body element
+const body = $('body');
+
+const questionsContainer = 
+$('<div class="questions-container">HELLO JQUERY</div>');
+
+body.append(questionsContainer);
