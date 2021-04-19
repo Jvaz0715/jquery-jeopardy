cc491ad254fb884ede50b5ea1dc58fc26b769cf1
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-19 15:36:03 -0400

    makes 5x5 grid using jquery

--- main.js
+++ main.js
@@ -5,16 +5,18 @@ const questionsContainer = $('<div class="questions-container"></div>');
 
 body.append(questionsContainer);
 
-function createGrid() {
-    for (let rows = 0; rows < 5; rows++) {
-        for (let columns = 0; columns < 5; columns++) {
-            questionsContainer.append("<div class='grid'></div>");
-        };
-    };
-    $(".grid").width(960/5);
-    $(".grid").height(960/5);
-};
+const row1 = $('<div class="row row-1"><div class="column">100</div><div class="column">100</div><div class="column">100</div><div class="column">100</div><div class="column">100</div></div>');
 
-body.ready(function (){
-    createGrid();
-})
+const row2 = $('<div class="row row-2"><div class="column">200</div><div class="column">200</div><div class="column">200</div><div class="column">200</div><div class="column">200</div></div>');
+
+const row3 = $('<div class="row row-3"><div class="column">300</div><div class="column">300</div><div class="column">300</div><div class="column">300</div><div class="column">300</div></div>');
+
+const row4 = $('<div class="row row-4"><div class="column">400</div><div class="column">400</div><div class="column">400</div><div class="column">400</div><div class="column">400</div></div>');
+
+const row5 = $('<div class="row row-5"><div class="column">500</div><div class="column">500</div><div class="column">500</div><div class="column">500</div><div class="column">500</div></div>');
+
+questionsContainer.append(row1);
+questionsContainer.append(row2);
+questionsContainer.append(row3);
+questionsContainer.append(row4);
+questionsContainer.append(row5);
