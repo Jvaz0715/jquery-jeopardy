fb767adbb1ac3efef8874e4a46f6dc0b73a1cff4
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-19 14:20:55 -0400

    Starts to add jquery for a 5x5 grid

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 // using jquery, get the body element
 const body = $('body');
 
-const questionsContainer = 
-$('<div class="questions-container">HELLO JQUERY</div>');
+const questionsContainer = $('<div class="questions-container"></div>');
 
 body.append(questionsContainer);
+
+function createGrid() {
+    for (let rows = 0; rows < 5; rows++) {
+        for (let columns = 0; columns < 5; columns++) {
+            questionsContainer.append("<div class='grid'></div>");
+        };
+    };
+    $(".grid").width(960/5);
+    $(".grid").height(960/5);
+};
+
+body.ready(function (){
+    createGrid();
+})
